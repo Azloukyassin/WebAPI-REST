d0496956c2e0eb04440278fb402a312b684bae0c
Azlouk Yassin <63109198+Azloukyassin@users.noreply.github.com>
2021-04-09 15:25:59 +0200

    Projektdateien hinzufügen.

---
+++ WebAPI-REST/App_Start/WebApiConfig.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace WebAPI_REST
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web-API-Konfiguration und -Dienste
+
+            // Web-API-Routen 
+            // Attribute-based routing 
+            config.MapHttpAttributeRoutes();
+
+
+
+            // convention-based routing 
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            config.Routes.MapHttpRoute(
+                name: "myRoute",
+                routeTemplate:"api/{controller}/{action}/{year}/{month}/{day}",
+                defaults:new
+                {
+                    month=RouteParameter.Optional,
+                    day=RouteParameter.Optional
+                }, 
+                constraints:new
+                {
+                    month=@"\d{0,2}",
+                    day= @"\d{0,2}"
+                }
+                
+
+                );
+        }
+    }
+}
---
+++ WebAPI-REST/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace WebAPI_REST.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
